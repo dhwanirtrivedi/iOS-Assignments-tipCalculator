4fe9705825277f8902830b235b2af03cb902a553
dhwanirtrivedi <dhwanirtrivedi@gmail.com>
2014-01-12 17:58:09 -0800

    Initial Commit

---
+++ tipcalculator/tipcalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Dhwani Trivedi on 1/12/14.
+//  Copyright (c) 2014 Dhwani Trivedi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
